73cc1a006e29bd486734d6c5690efdb9a5d7738f
Jimmy Chavada <rchavada@uwo.ca>
2024-07-03 11:14:59 -0400

    Update api.py

--- reportredcuer/api/api.py
+++ reportredcuer/api/api.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import PyPDF2
-import openai  # Import OpenAIClient from openai module
 import os
 from dotenv import load_dotenv
 
@@ -10,11 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/uploadFile": {"origins": "*"}})
 
-# Access the API key from the environment variables
-api_key = os.getenv("OPENAI_API_KEY")
 
-# Set the API key for the openai module
-openai.api_key = api_key
 
 @app.route("/uploadFile", methods=["POST", "GET"])
 def upload_file():
@@ -34,23 +29,13 @@ def upload_file():
         for page_number in range(num_pages):
             page_text = reader.pages[page_number].extract_text()
             extracted_text.append(page_text)
-        
-        # Make a request to OpenAI API using the created client instance
-        # Replace this with your actual OpenAI API request
-        res = openai.ChatCompletion.create ( 
-            model="gpt-3.5-turbo",
-            messages = [
-                {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-                {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-            ]
-        )
-
-        print(res)
 
         # Write extracted text to a file 
         with open("Data.txt", "w") as f:
             for text in extracted_text:
                 f.write(text + "\n")
+        
+
         
         return jsonify({
             "message": "File uploaded successfully",
